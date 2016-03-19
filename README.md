d61ac7ff60a6232cc123f9328e15fab3d401eeaa
sago35 <sago35@gmail.com>
2016-03-19 14:08:21 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Getopt::Kingpin - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::Kingpin;
+
+# DESCRIPTION
+
+Getopt::Kingpin is ...
+
+# LICENSE
+
+Copyright (C) sago35.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sago35 <sago35@gmail.com>
---
+++ lib/Getopt/Kingpin.pm
@@ -0,0 +1,39 @@
+package Getopt::Kingpin;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::Kingpin - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::Kingpin;
+
+=head1 DESCRIPTION
+
+Getopt::Kingpin is ...
+
+=head1 LICENSE
+
+Copyright (C) sago35.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sago35 E<lt>sago35@gmail.comE<gt>
+
+=cut
+
