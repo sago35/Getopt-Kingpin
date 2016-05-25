0274942525840269be44c1c6897ef2c46c32aee8
sago35 <sago35@gmail.com>
2016-05-25 20:29:19 +0900

    Remove unused condition (Command::help)

--- lib/Getopt/Kingpin/Command.pm
+++ lib/Getopt/Kingpin/Command.pm
@@ -40,12 +40,8 @@ sub help {
 
     printf "%s\n", $self->flags->help;
 
-    if ($self->commands->count > 1) {
-        printf "%s\n", $self->commands->help;
-    } else {
-        if ($self->args->count > 0) {
-            printf "%s\n", $self->args->help;
-        }
+    if ($self->args->count > 0) {
+        printf "%s\n", $self->args->help;
     }
 }
 
