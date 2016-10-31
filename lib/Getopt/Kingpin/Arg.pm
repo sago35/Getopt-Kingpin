4ba3caa51e21bc7bb7c6fbcd503ab842f530832b
sago35 <sago35@gmail.com>
2016-11-01 00:15:06 +0900

    Support perl 5.8 (cont.)

--- lib/Getopt/Kingpin/Arg.pm
+++ lib/Getopt/Kingpin/Arg.pm
@@ -8,7 +8,11 @@ our $VERSION = "0.04";
 
 sub help_name {
     my $self = shift;
-    my $mode = shift // 0;
+    my $mode = shift;
+
+    if (not defined $mode) {
+        $mode = 0;
+    }
 
     my $ret = '<' . $self->name . '>';
     if ($mode and $self->is_cumulative) {
